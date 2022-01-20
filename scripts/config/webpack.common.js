c3a4c9c3e66a9dfd670d8d2db9c439a02fe1a0da
dqdcma <dqcmcc@gmail.com>
2022-01-20 15:02:19 +0800

    chore: add commitlint to force commit style

---
+++ scripts/config/webpack.common.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, '../../src/app.js'),
+  },
+  output: {
+    filename: 'js/[name].[hash:8].js',
+    path: path.resolve(__dirname, '../../dist'),
+  },
+}
